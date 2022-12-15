c832d4ca1c086abb6cce75fe60a3bd51173e3824
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-12-15 14:07:47 -0700

    Include Y-Axis alongside graph

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ $(function() {
 
   function drawBarChart(data, options, element) {
     const max = Math.max(...data);
+
+    const barInfo = document.createElement("div");
+    $(barInfo).css({
+      'display': 'flex',
+      'flex-direction': 'row'
+    });
+    // Create bars for container based on data
     const barContainer = document.createElement("div");
     $(barContainer).addClass('bar-container');
     for (const datum of data) {
@@ -18,7 +25,24 @@ $(function() {
       bar.innerHTML = datum;
       $(barContainer).append(bar);
     }
-    $(element).append(barContainer);
+
+    // Create y-axis information
+    const yAxis = document.createElement('div');
+    $(yAxis).css({
+      'border-right': '2px solid black',
+      'height': `${$(barContainer).height}`
+    });
+    const yAxisText = document.createElement('div');
+    yAxisText.innerHTML = 'Y Axis';
+    $(yAxisText).css({
+      'writing-mode': 'vertical-lr',
+      'text-align': 'center'
+    });
+
+    $(barInfo).append(yAxisText);
+    $(barInfo).append(yAxis);
+    $(barInfo).append(barContainer);
+    $(element).append(barInfo);
   }
 
   drawBarChart([1, 2, 3, 4], 1, '#barchart-container');
